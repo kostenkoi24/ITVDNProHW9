edcb77e0157abfcd8b6f9b55856b50902fef96f5
kostenkoi24 <kostenko.i@gmail.com>
2023-02-14 23:35:29 +0200

    Homework9

--- Homework9_2/Program.cs
+++ Homework9_2/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-
+using System.Text;
+using System.Threading;
 
 namespace Homework9_2
 {
 
+    class BigObject
+    {
+        
+        Array array = new int[10000000];
+        public BigObject()
+        {
+            //Console.WriteLine(this.GetHashCode());
+        }
+
+        //~BigObject()
+        //{
+        //    Console.WriteLine("Об'єкт " + this.GetHashCode() + " видалено");
+        //}
+    }
+
+
+
+
     class MemoryMonitor
     {
 
-        void ShowAlert()
+        public void ShowAlert(object maxMemoryUsage)
         {
+            while (true)
+            {
+                Console.WriteLine("Monitoring...");
+                int memoryUsage = (int)GC.GetTotalMemory(false) / 1024 / 1024;
+                Console.WriteLine("Розмір купи = {0} MB", memoryUsage);
 
+                if (memoryUsage >= (int)maxMemoryUsage)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Monitoring - memoryUsage > maxMemory");
+                    Console.ResetColor();
+                    Thread.Sleep(1000000);
+                    
+                }
+
+                
+            }
+            
         }
 
     }
@@ -18,13 +54,51 @@ namespace Homework9_2
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+            /*
+             Створіть клас, який дозволить виконувати моніторинг ресурсів, 
+            що використовуються програмою. Використовуйте його з метою 
+            спостереження за роботою програми, а саме: користувач може вказати прийнятні 
+            рівні споживання ресурсів (пам'яті), а методи класу дозволять видати попередження, 
+            коли кількість ресурсів, що реально використовуються, наблизитися 
+            до максимально допустимого рівня.
+             */
+
+            MemoryMonitor memoryMonitor = new MemoryMonitor();
 
             Console.WriteLine("Input maximum memory usage in MB:");
 
             int maxMemoryUsage = int.Parse(Console.ReadLine());
 
+            ParameterizedThreadStart monitor = new ParameterizedThreadStart(memoryMonitor.ShowAlert);
 
+            Thread thread = new Thread(monitor);
+            thread.Start(maxMemoryUsage);
 
+            BigObject[] objects = new BigObject[1000];
+
+            int i = 0;
+
+            while (thread.ThreadState == ThreadState.WaitSleepJoin ? false : true )
+            {
+                
+                try
+                {
+                    
+                        Console.WriteLine("Add object to memory");
+                        objects[i] = new BigObject();
+                        i++;
+                        Thread.Sleep(1000);
+
+                   
+                }
+                catch (OutOfMemoryException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+            
 
             Console.ReadKey();
         }
--- Homework9_4/Program.cs
+++ Homework9_4/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
 
 namespace Homework9_4
 {
 
-    class BigObject
+    class BigObject : IDisposable
     {
         // Справді великий об'єкт. Буде розміщений у великій купі.
         // 100 000 000 * 4 Б = 400 000 000 Б = 390 625 КБ = 381 МБ
@@ -13,33 +14,55 @@ namespace Homework9_4
         {
             Console.WriteLine(this.GetHashCode());
         }
-        ~BigObject()
+
+        public void Dispose()
         {
-            Console.WriteLine("Об'єкт " + this.GetHashCode() + " видалено");
+            Console.WriteLine($"{this.GetHashCode()} has been disposed"); //реалізуйте для цього класу формалізований шаблон очищення.
         }
+
+        //~BigObject()
+        //{
+        //    Console.WriteLine("Об'єкт " + this.GetHashCode() + " видалено");
+        //}
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            /*Створіть свій клас, об'єкти якого займатимуть багато місця в пам'яті 
+             * (наприклад, у коді класу буде великий масив) і реалізуйте для цього класу формалізований шаблон очищення.
+             */
+
             Console.OutputEncoding = Encoding.Unicode;
             // Масив об'єктів - BigObject.
             // 381 * 1000 = 381 000 МБ = 372 ГБ - розмір всього масиву.
-            BigObject[] objects = new BigObject[1000];
+            BigObject[] objects = new BigObject[10];
 
             try
             {
                 for (int i = 0; i < objects.Length; i++)
                 {
-                    //objects[i] = new BigObject();
-                    BigObject @object = new BigObject();
+                    objects[i] = new BigObject();
+                    Console.WriteLine("Розмір купи = {0} MB", GC.GetTotalMemory(false) / 1024 / 1024); // true
+                    //Thread.Sleep(1000);
+                    //BigObject @object = new BigObject();
                 }
             }
             catch (OutOfMemoryException ex)
             {
                 Console.WriteLine(ex.Message);
             }
+
+            
+            
+
+            foreach (BigObject big in objects)
+            {
+                big.Dispose(); // очистка памяти
+            }
+            
+
 
             // Delay.
             Console.ReadKey();
